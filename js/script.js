c828d736cab40c83d167ba7516ab38bf903f5e9c
Nikita <nikitka_ugatu@mail.ru>
2022-11-22 22:00:51 +0300

    refactoring

--- js/script.js
+++ js/script.js
@@ -5,38 +5,40 @@ const tasksList = document.querySelector('#tasksList');
 const emptyList = document.querySelector('#emptyList');
 
 
-form.addEventListener('submit', (e) => {
-    // Отмена отправки формы
-    e.preventDefault();
+form.addEventListener('submit', addTask)
+
+function addTask(e) {
+ // Отмена отправки формы
+ e.preventDefault();
 
 
-    // Вытаскивание текста задачи из поля ввода
-    const taskText = taskInput.value
+ // Вытаскивание текста задачи из поля ввода
+ const taskText = taskInput.value
 
-    // Разметка для новой задачи
-    const taskHTML = 
-    `
-    <li class="list-group-item task-item d-flex justify-content-between">
-        <span class="task-title">${taskText}</span>
-        <div class="task-title_buttons">
-            <button type="button" data-action="done" class="btn-action">
-                <img src="./img/tick.svg" alt="Done" width="18" height="18">
-            </button>
-            <button type="button" data-action="delete" class="btn-action">
-                <img src="./img/cross.svg" alt="Delete" width="18" height="18">
-            </button>
-        </div>
-    </li>`;
+ // Разметка для новой задачи
+ const taskHTML = 
+ `
+ <li class="list-group-item task-item d-flex justify-content-between">
+     <span class="task-title">${taskText}</span>
+     <div class="task-title_buttons">
+         <button type="button" data-action="done" class="btn-action">
+             <img src="./img/tick.svg" alt="Done" width="18" height="18">
+         </button>
+         <button type="button" data-action="delete" class="btn-action">
+             <img src="./img/cross.svg" alt="Delete" width="18" height="18">
+         </button>
+     </div>
+ </li>`;
 
-    // Добавление задачи на страницу
-    tasksList.insertAdjacentHTML('beforeend', taskHTML);
+ // Добавление задачи на страницу
+ tasksList.insertAdjacentHTML('beforeend', taskHTML);
 
-    // Очистка input и возврат фокуса
-    taskInput.value = "";
-    taskInput.focus();
+ // Очистка input и возврат фокуса
+ taskInput.value = "";
+ taskInput.focus();
 
-    // Скрытие emptyList
-    if(tasksList.children.length > 1) {
-        emptyList.classList.add('none');
-    }
-})
+ // Скрытие emptyList
+ if(tasksList.children.length > 1) {
+     emptyList.classList.add('none');
+ }
+}
