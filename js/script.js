c6ddd5773cffd126603c6e09145342365fb595f2
Nikita <nikitka_ugatu@mail.ru>
2022-11-22 20:57:47 +0300

    adding task

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Поиск элементов на страницы
 const form =  document.querySelector('#form');
 const taskInput = document.querySelector('#taskInput');
+const tasksList = document.querySelector('#tasksList');
 
 
 form.addEventListener('submit', (e) => {
@@ -10,5 +11,22 @@ form.addEventListener('submit', (e) => {
 
     // Вытаскивание текста задачи из поля ввода
     const taskText = taskInput.value
+
+    // Разметка для новой задачи
+    const taskHTML = 
+    `
+    <li class="list-group-item task-item d-flex justify-content-between">
+        <span class="task-title">${taskText}</span>
+        <div class="task-title_buttons">
+            <button type="button" data-action="done" class="btn-action">
+                <img src="./img/tick.svg" alt="Done" width="18" height="18">
+            </button>
+            <button type="button" data-action="delete" class="btn-action">
+                <img src="./img/cross.svg" alt="Delete" width="18" height="18">
+            </button>
+        </div>
+    </li>`;
+
+    // Добавление задачи на страницу
 
 })
