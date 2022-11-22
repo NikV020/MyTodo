527d0a39deb41d66002301c223757e9a04c249c1
Nikita <nikitka_ugatu@mail.ru>
2022-11-22 20:46:33 +0300

    text in input

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 // Поиск элементов на страницы
 const form =  document.querySelector('#form');
 const taskInput = document.querySelector('#taskInput');
+
+
+form.addEventListener('submit', (e) => {
+    // Отмена отправки формы
+    e.preventDefault();
+
+
+    // Вытаскивание текста задачи из поля ввода
+    const taskText = taskInput.value
+
+})
